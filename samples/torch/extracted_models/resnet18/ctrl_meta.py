c523e0d933afda9319d1051df158242ccb4e8645
JewelRoam <2752594773@qq.com>
2025-07-23 11:28:43 +0800

    remove samples

--- samples/torch/extracted_models/resnet18/ctrl_meta.py
+++
@@ -1,8 +0,0 @@
-class Program_ctrl_tensor_meta_be773aa8901c4c639de2c545b67fd087:
-	name = "s1"
-	shape = []
-	dtype = "torch.int64"
-	device = "cpu"
-	mean = None
-	std = None
-	data = [0]
--- samples/torch/extracted_models/resnet18/input_meta.py
+++
@@ -1,8 +0,0 @@
-class Program_input_tensor_meta_44eed49fb5bb479e8b2c7988577cd099:
-	name = "input_0"
-	shape = [64, 3, 7, 7]
-	dtype = "torch.float32"
-	device = "cpu"
-	mean = 2.941965612990316e-05
-	std = 0.1296982318162918
-	data = None

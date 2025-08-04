36e5897bea398948be155c5e4d1126516c6aead0
liumengyuan <liumengyuan@mail.ustc.edu.cn>
2025-08-04 23:09:57 +0800

    Add inception_resnet_v2

--- graph_net/test/vision_model_test.py
+++ graph_net/test/vision_model_test.py
@@ -1,4 +1,5 @@
 import argparse
+import timm
 import os
 import json
 import torch
@@ -6,8 +7,13 @@ import torchvision
 from torchvision import transforms
 import os
 import graph_net
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", type=str, default="efficientnet_v2_s")
+    args = parser.parse_args()
+    model_name = args.model_name
     # Normalization parameters for ImageNet
     normalize = transforms.Normalize(
         mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
@@ -21,14 +27,23 @@ if __name__ == "__main__":
     normalized_input = normalize(random_input)
 
     # Instantiate model
-    model = torchvision.models.get_model("resnet18", weights="DEFAULT")
+    try:
+        model = torchvision.models.get_model(model_name, weights="DEFAULT")
+    except:
+        print(f"trying find model {model_name} in timm")
+        model = timm.create_model(model_name, pretrained=False)
+
     model.eval()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
     normalized_input = normalized_input.to(device)
 
+<<<<<<< Updated upstream
     model = graph_net.torch.extract(name="resnet18", dynamic=True)(model)
+=======
+    model = graph_net.torch.extract(name=model_name)(model)
+>>>>>>> Stashed changes
 
     print("Running inference...")
     print("Input shape:", normalized_input.shape)

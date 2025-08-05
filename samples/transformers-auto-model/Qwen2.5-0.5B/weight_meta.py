9fba76be7981f2ee7dc1b8b18e56ef1eec744c89
fangfangssj <1135470306@qq.com>
2025-08-05 11:29:12 +0800

    fix codestyle

--- samples/transformers-auto-model/Qwen2.5-0.5B/weight_meta.py
+++ samples/transformers-auto-model/Qwen2.5-0.5B/weight_meta.py
@@ -1,35 +1,38 @@
 class Program_weight_tensor_meta_s0:
-	name = "s0"
-	shape = []
-	dtype = "torch.int64"
-	device = "cpu"
-	mean = None
-	std = None
-	data = [4]
+    name = "s0"
+    shape = []
+    dtype = "torch.int64"
+    device = "cpu"
+    mean = None
+    std = None
+    data = [4]
+
 
 class Program_weight_tensor_meta_L_stack0_last_hidden_state:
-	name = "L_stack0_last_hidden_state"
-	shape = [1, 5, 896]
-	dtype = "torch.float32"
-	device = "cpu"
-	mean = 0.443
-	std = 9.962
-	data = None
+    name = "L_stack0_last_hidden_state"
+    shape = [1, 5, 896]
+    dtype = "torch.float32"
+    device = "cpu"
+    mean = 0.443
+    std = 9.962
+    data = None
+
 
 class Program_weight_tensor_meta_L_logits_to_keep_:
-	name = "L_logits_to_keep_"
-	shape = []
-	dtype = "torch.int64"
-	device = "cpu"
-	mean = None
-	std = None
-	data = [4]
+    name = "L_logits_to_keep_"
+    shape = []
+    dtype = "torch.int64"
+    device = "cpu"
+    mean = None
+    std = None
+    data = [4]
+
 
 class Program_weight_tensor_meta_L_self_modules_lm_head_parameters_weight_:
-	name = "L_self_modules_lm_head_parameters_weight_"
-	shape = [151936, 896]
-	dtype = "torch.float32"
-	device = "cpu"
-	mean = 0.000
-	std = 0.016
-	data = None
+    name = "L_self_modules_lm_head_parameters_weight_"
+    shape = [151936, 896]
+    dtype = "torch.float32"
+    device = "cpu"
+    mean = 0.000
+    std = 0.016
+    data = None

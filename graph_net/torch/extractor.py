b54616cfa1e1dc0538b4aedc968a3ce1b51310f9
fangfangssj <1135470306@qq.com>
2025-08-07 15:33:27 +0800

    fix

--- graph_net/torch/extractor.py
+++ graph_net/torch/extractor.py
@@ -6,6 +6,7 @@ from . import utils
 
 torch._dynamo.config.capture_scalar_outputs = True
 
+
 def extract(name, dynamic=True, mut_graph_codes=None, placeholder_auto_rename=False):
     """
     A decorator for PyTorch functions to capture the computation graph.

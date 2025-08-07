93342ee1736c265fc8c2e7886fd6fd07aa555b92
Wang Xin <xinwang614@gmail.com>
2025-08-07 01:04:27 +0000

    [New Sample] Add `microsoft deberta large mnli`

---
+++ samples/transformers-auto-model/microsoft_deberta_large_mnli/weight_meta.py
@@ -0,0 +1,68 @@
+class Program_weight_tensor_meta_L_stack0_0_:
+    name = "L_stack0_0_"
+    shape = [1, 10, 1024]
+    dtype = "torch.float32"
+    device = "cuda:0"
+    mean = -0.006
+    std = 1.098
+    data = None
+
+
+class Program_weight_tensor_meta_L_self_modules_intermediate_modules_dense_parameters_weight_:
+    name = "L_self_modules_intermediate_modules_dense_parameters_weight_"
+    shape = [4096, 1024]
+    dtype = "torch.float32"
+    device = "cuda:0"
+    mean = -0.000
+    std = 0.026
+    data = None
+
+
+class Program_weight_tensor_meta_L_self_modules_intermediate_modules_dense_parameters_bias_:
+    name = "L_self_modules_intermediate_modules_dense_parameters_bias_"
+    shape = [4096]
+    dtype = "torch.float32"
+    device = "cuda:0"
+    mean = -0.033
+    std = 0.022
+    data = None
+
+
+class Program_weight_tensor_meta_L_self_modules_output_modules_dense_parameters_weight_:
+    name = "L_self_modules_output_modules_dense_parameters_weight_"
+    shape = [1024, 4096]
+    dtype = "torch.float32"
+    device = "cuda:0"
+    mean = 0.000
+    std = 0.026
+    data = None
+
+
+class Program_weight_tensor_meta_L_self_modules_output_modules_dense_parameters_bias_:
+    name = "L_self_modules_output_modules_dense_parameters_bias_"
+    shape = [1024]
+    dtype = "torch.float32"
+    device = "cuda:0"
+    mean = 0.000
+    std = 0.014
+    data = None
+
+
+class Program_weight_tensor_meta_L_self_modules_output_modules_LayerNorm_parameters_weight_:
+    name = "L_self_modules_output_modules_LayerNorm_parameters_weight_"
+    shape = [1024]
+    dtype = "torch.float32"
+    device = "cuda:0"
+    mean = 1.131
+    std = 0.063
+    data = None
+
+
+class Program_weight_tensor_meta_L_self_modules_output_modules_LayerNorm_parameters_bias_:
+    name = "L_self_modules_output_modules_LayerNorm_parameters_bias_"
+    shape = [1024]
+    dtype = "torch.float32"
+    device = "cuda:0"
+    mean = -0.001
+    std = 0.032
+    data = None

a5a172afefdbe9e6fe7b3bed1d4a25df7eef41b2
JewelRoam <2752594773@qq.com>
2025-08-29 12:14:26 +0000

    Update

--- graph_net/benchmark_demo.sh
+++ graph_net/benchmark_demo.sh
@@ -7,8 +7,14 @@ for i in /work/GraphNet/samples/torchvision/*/; do
     
     python -m graph_net.torch.test_compiler \
         --model-path "${i}" \
+        --compiler "inductor" \
+        --warmup 3 \
+        --trials 10 \
         --device cuda \
+        --output-dir "/work/GraphNet/benchmark_logs"
        
 done  > torchvision_cuda.log 2>&1
 
-python3 -m graph_net.analysis --benchmark-log-file /work/GraphNet/torchvision_cuda.log
+# python3 -m graph_net.analysis --test-compiler-log-file /work/GraphNet/torchvision_cuda.log
+
+python3 -m graph_net.analysis --benchmark-path /work/GraphNet/benchmark

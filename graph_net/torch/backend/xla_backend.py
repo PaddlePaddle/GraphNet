8c7262555363b4d720baa6d223f9c682d4be951b
fangfangssj <1135470306@qq.com>
2025-09-05 16:05:06 +0800

    fix bug

--- graph_net/torch/backend/xla_backend.py
+++ graph_net/torch/backend/xla_backend.py
@@ -7,11 +7,28 @@ except ImportError:
     torch_xla = None
 
 
+class XlaCompiledModule(torch.nn.Module):
+    def __init__(self, module):
+        super().__init__()
+        self.module = module
+        self.counter = 0
+        self.xla_input = {}
+
+    def forward(self, **kwargs):
+        if self.counter == 0:
+            self.xla_input = {k: v.to("xla") for k, v in kwargs.items()}
+            self.module = torch.compile(self.module, backend="openxla")
+
+        ret = self.module(**self.xla_input)
+        self.counter += 1
+        return ret
+
+
 class XlaBackend(GraphCompilerBackend):
     def __call__(self, model):
         if torch_xla is None:
             raise ImportError("torch_xla not installed")
-        return torch.compile(model, backend="openxla")
+        return XlaCompiledModule(model)
 
     def synchronize(self):
         torch_xla.sync()

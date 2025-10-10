8808181628263d930d067bf115900b0052948b19
JewelRoam <2752594773@qq.com>
2025-10-10 11:11:41 +0000

    Update

--- graph_net/config/datatype_tolerance_config.py
+++ graph_net/config/datatype_tolerance_config.py
@@ -1,11 +1,13 @@
 def get_precision(t, dtype):
-    if "float16" in dtype:
-        return 10 ** (t * 3 / 5), 10**t
-    elif "bfloat16" in dtype:
-        return 10 ** (t * 1.796 / 5), 10**t
-    elif "float32" in dtype:
-        return 10 ** (t * 5.886 / 5), 10**t
-    elif "float64" in dtype:
-        return 10 ** (t * 7 / 5), 10 ** (t * 7 / 5)
-    else:
-        return 0, 0
+    dtype_map = {
+        "float16": (3 / 5, 1),
+        "bfloat16": (1.796 / 5, 1),
+        "float32": (5.886 / 5, 1),
+        "float64": (7 / 5, 7 / 5),
+    }
+
+    for key, (exp1, exp2) in dtype_map.items():
+        if dtype == key:
+            return 10 ** (t * exp1), 10 ** (t * exp2)
+
+    return 0, 0

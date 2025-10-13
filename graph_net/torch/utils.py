3b549da1409099f33a82bbaf0a74154be7da0367
Liu Yiqun <liuyiqun01@baidu.com>
2025-10-13 11:20:10 +0800

    Add TODO.

--- graph_net/torch/utils.py
+++ graph_net/torch/utils.py
@@ -272,6 +272,7 @@ def replay_tensor(info):
     if dtype is torch.bool:
         return (torch.randn(size=shape) > 0.5).to(dtype).to(device)
     tensor = torch.randn(size=shape).to(dtype).to(device) * std * 0.2 + mean
+    # TODO(Xreki): remove this ugly code, and change the weight_meta instead.
     if name.startswith("L_self_modules") and "buffers_running_var" in name:
         tensor = torch.clip(tensor, min=0)
     return tensor

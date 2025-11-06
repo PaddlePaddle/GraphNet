1e4381a0084ed5a8705e8eeecda915361e311485
Gruge <202421090105@std.uestc.edu.cn>
2025-11-06 14:59:11 +0800

    update code

--- graph_net/torch/backend/unstable_to_stable_backend.py
+++ graph_net/torch/backend/unstable_to_stable_backend.py
@@ -57,15 +57,17 @@ class UnstableToStableBackend(GraphCompilerBackend):
         Convert torch._C._fft.fft_fftn to torch.fft.fftn
         """
         # Update graph nodes: replace torch._C._fft.fft_fftn with torch.fft.fftn
-        for node in gm.graph.nodes:
-            if node.op == "call_function":
-                if (
-                    hasattr(node.target, "__module__")
-                    and hasattr(node.target, "__name__")
-                    and node.target.__module__ == "torch._C._fft"
-                    and node.target.__name__ == "fft_fftn"
-                ):
-                    node.target = torch.fft.fftn
+        issue_nodes = (
+            node
+            for node in gm.graph.nodes
+            if node.op == "call_function"
+            if hasattr(node.target, "__module__")
+            if node.target.__module__ == "torch._C._fft"
+            if hasattr(node.target, "__name__")
+            if node.target.__name__ == "fft_fftn"
+        )
+        for node in issue_nodes:
+            node.target = torch.fft.fftn
 
         # Recompile the graph
         gm.recompile()

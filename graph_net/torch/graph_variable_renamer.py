b699698659dc44dc9fe3246582021816dae9cb25
TelGome <2657726985@qq.com>
2025-12-08 19:03:29 +0800

    fix

--- graph_net/torch/graph_variable_renamer.py
+++ graph_net/torch/graph_variable_renamer.py
@@ -78,8 +78,9 @@ class GraphVariableRenamer:
         module, inputs = get_torch_module_and_inputs(src_model_path)
         gm = parse_sole_graph_module(module, inputs)
         gm = self.rename_graph_variables(gm, inputs, src_model_path)
-        # print(gm)
-        dst_model_path = os.path.join(self.config["output_dir"], rel_model_path)
+        dst_model_path = os.path.realpath(
+            os.path.join(self.config["output_dir"], rel_model_path)
+        )
         Path(dst_model_path).parent.mkdir(parents=True, exist_ok=True)
         shutil.copytree(src_model_path, dst_model_path, dirs_exist_ok=True)
         self._update_model_py_file(gm, dst_model_path)

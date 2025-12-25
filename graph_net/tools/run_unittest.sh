e6a0579fecadbde24120dbf9d6c01e6f84c3e81f
roll-away <220250881@seu.edu.cn>
2025-12-25 06:24:53 +0000

    fix graph_net/tools/run_unittest.sh

--- graph_net/tools/run_unittest.sh
+++ graph_net/tools/run_unittest.sh
@@ -3,16 +3,45 @@ set -e
 export GRAPH_NET_ROOT=$(cd $(dirname "$0")/../.. && pwd)
 export PYTHONPATH=${GRAPH_NET_ROOT}:$PYTHONPATH
 
-UNITTEST_PATH="$GRAPH_NET_ROOT/graph_net/torch/unittest"
+function LOG {
+  echo "[$0:${BASH_LINENO[0]}] $*" >&2
+}
 
-python3.10 -m unittest discover \
-    -s "$UNITTEST_PATH" \
-    -p "test_*.py"
-RET=$?
+function prepare_torch_env() {
+  git config --global --add safe.directory "*"
+  num_changed_samples=$(git diff --diff-filter=ACM --name-only develop | grep -E "\bsamples\b/.*/(model\.py|input_meta\.py|weight_meta\.py)$" | wc -l)
+  if [ ${num_changed_samples} -ne 0 ]; then
+    LOG "[INFO] Device Id: ${CUDA_VISIBLE_DEVICES}"
+    # Update pip
+    LOG "[INFO] Update pip ..."
+    env http_proxy="" https_proxy="" python3.10 -m pip install -U pip > /dev/null
+    [ $? -ne 0 ] && LOG "[FATAL] Update pip failed!" && exit -1
+    # install torch
+    python3.10 -m pip install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu126 > /dev/null
+    [ $? -ne 0 ] && LOG "[FATAL] Install torch2.9.0 failed!" && exit -1
+  else
+    LOG "[INFO] This pull request doesn't change any torch samples, skip the CI."
+  fi
+}
+function run_unit_test() {
+    UNITTEST_PATH="$GRAPH_NET_ROOT/graph_net/torch/unittest"
 
-if [ $RET -eq 0 ]; then
-    echo "All unit tests passed!"
-else
-    echo "Unit tests failed with exit code $RET"
-fi
-exit $RET
+    python3.10 -m unittest discover \
+        -s "$UNITTEST_PATH" \
+        -p "test_*.py"
+    RET=$?
+
+    if [ $RET -eq 0 ]; then
+        echo "All unit tests passed!"
+    else
+        echo "Unit tests failed with exit code $RET"
+    fi
+    return $RET
+}
+
+function main() {
+    prepare_torch_env
+    run_unit_test
+}
+
+main

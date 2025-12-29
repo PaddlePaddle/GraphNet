62caeba0f4dc7a8fccb09ffc3558243e92148a17
roll-away <220250881@seu.edu.cn>
2025-12-29 03:34:54 +0000

    fix the bug: The expanded size of the tensor doesn't match

--- graph_net/test/run_model.sh
+++ graph_net/test/run_model.sh
@@ -3,13 +3,10 @@
 GRAPH_NET_ROOT=$(python3 -c "import graph_net; import os; print(
 os.path.dirname(graph_net.__file__))")
 
-# input model path
-# MODEL_NAME=opus-mt-en-gv
-# MODEL_PATH_PREFIX=$GRAPH_NET_ROOT/../samples/transformers-auto-model/
-MODEL_NAME=opus-mt-en-gv__S0_128_S1_64
-MODEL_PATH_PREFIX=/tmp/dimension_generalized_samples/samples/transformers-auto-model/opus-mt-en-gv
-MODEL_PATH=$MODEL_PATH_PREFIX/$MODEL_NAME
+MODEL_LIST_FILE="/work/.BCloud/run_model_use_small_sample_list.log"
+RUN_LOG="/work/.BCloud/run_model_samll.log"
 
-DECORATOR_CONFIG=$(echo $decorator_config_json_str | base64 -w 0)
-
-python3 -m graph_net.torch.run_model --model-path $MODEL_PATH
+while IFS= read -r FULL_PATH || [[ -n "$FULL_PATH" ]]
+do
+    python3 -m graph_net.torch.run_model --model-path "$FULL_PATH" >> "$RUN_LOG" 2>&1
+done < "$MODEL_LIST_FILE"
